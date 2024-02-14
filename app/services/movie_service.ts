9e425a01225a9ea8422683af9e0decc21e1b6f07
Matteo Gassend <matteo.gassend@agiltech.fr>
2024-02-14 10:40:25 +0100

    add: update package and type

--- app/services/movie_service.ts
+++ app/services/movie_service.ts
@@ -1,4 +1,4 @@
-import API from '@matfire/the_movie_wrapper'
+import API, { Language } from '@matfire/the_movie_wrapper'
 
 import { bento } from '#config/bento'
 import env from '#start/env'
@@ -6,7 +6,7 @@ import env from '#start/env'
 export default class MovieService {
   static async getMovie(id: number, lang = 'en') {
     const tmdbClient = await new API(env.get('TMDB_API_KEY'))
-    tmdbClient.setLanguage(lang)
+    tmdbClient.setLanguage(lang as Language)
     const data = await bento.getOrSet(`${id}:${lang}`, () => tmdbClient.movies.getMovie(id))
     return data
   }
