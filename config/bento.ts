16ea92221547e7b63040e64f4f4ef12af75590e9
Matteo Gassend <mgassend@gmail.com>
2024-02-16 16:07:07 +0100

    add: l2 cache config with upstash

--- config/bento.ts
+++ config/bento.ts
@@ -1,11 +1,22 @@
+import env from '#start/env'
 import { BentoCache, bentostore } from 'bentocache'
 import { memoryDriver } from 'bentocache/drivers/memory'
+import { redisDriver } from 'bentocache/drivers/redis'
+import { Redis } from 'ioredis'
+
+const redisConnection = new Redis(
+  `rediss://${env.get('REDIS_USERNAME')}:${env.get('REDIS_PASSWORD')}@${env.get('REDIS_ENDPOINT')}:${env.get('REDIS_PORT')}");`
+)
 
 export const bento = new BentoCache({
   default: 'movies',
   stores: {
-    // A first cache store named "myCache" using
-    // only L1 in-memory cache
-    movies: bentostore().useL1Layer(memoryDriver({ maxSize: 10_000 })),
+    movies: bentostore({ ttl: '24h' })
+      .useL1Layer(memoryDriver({ maxSize: 10_000 }))
+      .useL2Layer(
+        redisDriver({
+          connection: redisConnection,
+        })
+      ),
   },
 })
