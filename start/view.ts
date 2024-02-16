6c4ecedbe2b8b33a6dc838dd271bd527e49a5734
Matteo Gassend <mgassend@gmail.com>
2024-02-16 18:56:44 +0100

    add: home layout wip + ttranslation

--- start/view.ts
+++ start/view.ts
@@ -1,5 +1,8 @@
 import edge from 'edge.js'
-import { edgeIconify } from 'edge-iconify'
+import { edgeIconify, addCollection } from 'edge-iconify'
+import { icons as heroIcons } from '@iconify-json/heroicons'
+
+addCollection(heroIcons)
 
 /**
  * Register a plugin
