eda6fc9b9a5dc1610b56567bb3fb2a5135bc7f85
Matteo Gassend <mgassend@gmail.com>
2024-02-17 16:31:23 +0100

    add: mdi iconset

--- start/view.ts
+++ start/view.ts
@@ -1,8 +1,9 @@
 import edge from 'edge.js'
 import { edgeIconify, addCollection } from 'edge-iconify'
 import { icons as heroIcons } from '@iconify-json/heroicons'
-
+import { icons as mdiIcons } from '@iconify-json/mdi'
 addCollection(heroIcons)
+addCollection(mdiIcons)
 
 /**
  * Register a plugin
