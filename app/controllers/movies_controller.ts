1badd43b30fdb00fdce2cdfd0ff8bf9d1898767a
Matteo Gassend <matteo.gassend@agiltech.fr>
2024-02-26 16:11:46 +0100

    add: turbo + view transition meta tag

--- app/controllers/movies_controller.ts
+++ app/controllers/movies_controller.ts
@@ -2,8 +2,8 @@ import MovieService from '#services/movie_service'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class MoviesController {
-  async show({ view, params }: HttpContext) {
-    const movie = await MovieService.getMovie(params.id)
+  async show({ view, params, i18n }: HttpContext) {
+    const movie = await MovieService.getMovie(params.id, i18n.locale)
     return view.render('pages/app/movie/show', {
       movie,
     })
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,3 @@
 import '../css/app.css'
 import.meta.glob(['../img/**'])
+import * as Turbo from '@hotwired/turbo'
