86181bd8c317d8785d8d5a03a06db97be994d5fc
Matteo Gassend <matteo.gassend@agiltech.fr>
2024-02-26 15:58:15 +0100

    add:  register view transitions plugin

--- start/view.ts
+++ start/view.ts
@@ -1,4 +1,5 @@
 import edge from 'edge.js'
+import { edgeViewTransitions } from '@matfire/edge-view-transitions'
 import { edgeIconify, addCollection } from 'edge-iconify'
 import { icons as heroIcons } from '@iconify-json/heroicons'
 import { icons as mdiIcons } from '@iconify-json/mdi'
@@ -9,3 +10,4 @@ addCollection(mdiIcons)
  * Register a plugin
  */
 edge.use(edgeIconify)
+edge.use(edgeViewTransitions)
