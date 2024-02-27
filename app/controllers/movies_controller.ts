7a87a674a755fb2ddee8c275bc069f1b85275640
Matteo Gassend <matteo.gassend@agiltech.fr>
2024-02-27 11:50:18 +0100

    add: link to playlists containing movie

--- app/controllers/movies_controller.ts
+++ app/controllers/movies_controller.ts
@@ -1,8 +1,20 @@
+import Movie from '#models/movie'
+import Playlist from '#models/playlist'
 import MovieService from '#services/movie_service'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class MoviesController {
-  async show({ view, params, i18n }: HttpContext) {
+  async show({ view, params, i18n, auth }: HttpContext) {
+    const user = await auth.authenticate()
+    const movies = await Movie.query().where('tmdb_id', params.id)
+    const playlists = await Playlist.query()
+      .whereIn(
+        'id',
+        movies.map((e) => e.playlistId)
+      )
+      .andWhere('user_id', user.id)
+      .orWhere('is_public', true)
+
     const movie = await MovieService.getMovie(params.id, i18n.locale, [
       'credits',
       'videos',
@@ -11,6 +23,7 @@ export default class MoviesController {
     ])
     return view.render('pages/app/movie/show', {
       movie,
+      playlists,
     })
   }
 }
