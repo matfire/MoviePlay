c90b640b10728c273911211666eefff4ba587dcb
Matteo Gassend <matteo.gassend@agiltech.fr>
2024-04-10 09:53:32 +0200

    add: new logos

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ export default {
   darkMode: 'class',
   content: ['resources/views/**/*.{edge|html|js|jsx|ts|tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        "light": "#E0E0E0",
+        "dark": "#1E1D1E"
+      }
+    },
   },
   plugins: [require('@tailwindcss/container-queries'), require('tailwind-scrollbar'), require('@tailwindcss/forms')],
 }
