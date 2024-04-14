a9cc40179d65029ee9a21ae2cd1a0e7e61a9e467
Matteo Gassend <mgassend@gmail.com>
2024-04-14 15:15:10 +0200

    fix: remove unique constraint

--- database/migrations/1707587885897_create_movies_table.ts
+++ database/migrations/1707587885897_create_movies_table.ts
@@ -6,7 +6,7 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.integer('tmdb_id').unsigned().notNullable().unique()
+      table.integer('tmdb_id').unsigned().notNullable()
       table.timestamp('created_at')
       table.timestamp('updated_at')
     })
