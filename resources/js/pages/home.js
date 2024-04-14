c86434eb6327d8eef4bde8e3fb6b74b1a6915f5a
Matteo Gassend <mgassend@gmail.com>
2024-04-14 17:38:28 +0200

    add: more animation stuff

--- resources/js/pages/home.js
+++ resources/js/pages/home.js
@@ -1,21 +1,17 @@
-import { timeline, inView, animate } from "motion"
-const draw = (progress) => ({
-  strokeDashoffset: 1 - progress,
-  visibility: "visible"
-})
+import { timeline, inView, stagger } from "motion"
 
 document.querySelectorAll("section").forEach(el => {
   el.style.opacity = 0;
 })
+document.querySelectorAll(".feature").forEach((el) => {
+  el.style.opacity = 0
+})
+document.querySelector("#tmdb-logo").style.opacity = 0
 
 inView("section", (element) => {
-  animate(
-    element.target,
-    {
-      opacity: 100
-    },
-    {
-      delay: 0.3
-    }
-  )
+  timeline([
+    [element.target, { opacity: 100 }, { delay: 0.3 }],
+    [element.target.querySelectorAll(".feature"), { opacity: 100, x: [-100, 0] }, { delay: stagger(0.3), duration: 0.8 }],
+    [element.target.querySelector("#tmdb-logo"), { opacity: 100, y: [-100, 0] }, { delay: 0.3, duration: 0.5 }]
+  ])
 })
