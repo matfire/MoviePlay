ee919b8a5f7572dfe6819c4ba8067c6f3cda368b
Matteo Gassend <matteo.gassend@agiltech.fr>
2024-04-15 13:40:03 +0200

    add: app home rework wip

--- app/controllers/apps_controller.ts
+++ app/controllers/apps_controller.ts
@@ -1,24 +1,37 @@
 import Hit from '#models/hit'
 import Playlist from '#models/playlist'
+import MovieService from '#services/movie_service'
 import { HttpContext } from '@adonisjs/core/http'
+import { Movie } from '@matfire/the_movie_wrapper'
 
 export default class AppsController {
-  async index({ view, auth }: HttpContext) {
+  async index({ view, auth, i18n }: HttpContext) {
     const user = await auth.authenticate()
-    const playlists = await Playlist.query().where('userId', user.id)
+    const playlists = await Playlist.query().where('userId', user.id).preload("movies")
     const publicPlaylists = await Playlist.query()
       .whereNot('userId', user.id)
       .andWhere('isPublic', true)
+      .preload("movies")
     const hits = await Hit.query()
       .whereIn(
         'playlistId',
         playlists.map((e) => e.id)
       )
       .count('*', 'total')
+    const moviesData = new Map<number, Movie>()
+    await Promise.allSettled(playlists.map(async (p) =>
+      await Promise.allSettled(p.movies.map(async (m) => {
+        if (!moviesData.has(m.tmdbId)) {
+          moviesData.set(m.tmdbId, await MovieService.getMovie(m.tmdbId, i18n.locale))
+        }
+      }
+      ))
+    ))
     return view.render('pages/app/index', {
       playlists,
       hits: hits[0].$extras.total ?? 0,
       publicPlaylists,
+      moviesData
     })
   }
   async create({ view }: HttpContext) {
@@ -26,7 +39,7 @@ export default class AppsController {
   }
 
   async store({ request, response, auth }: HttpContext) {
-    const data = await request.all()
+    const data = request.all()
     const p = await Playlist.create({
       userId: auth.user?.id,
       isPublic: data.isPublic,
