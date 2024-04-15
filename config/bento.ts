cbe759c11427298b97e3de06b9ed1c7251ab7d46
Matteo Gassend <matteo.gassend@agiltech.fr>
2024-04-15 13:39:46 +0200

    del: removed redis driver

--- config/bento.ts
+++ config/bento.ts
@@ -1,22 +1,11 @@
 import env from '#start/env'
 import { BentoCache, bentostore } from 'bentocache'
 import { memoryDriver } from 'bentocache/drivers/memory'
-import { redisDriver } from 'bentocache/drivers/redis'
-import { Redis } from 'ioredis'
-
-const redisConnection = new Redis(
-  `rediss://${env.get('REDIS_USERNAME')}:${env.get('REDIS_PASSWORD')}@${env.get('REDIS_ENDPOINT')}:${env.get('REDIS_PORT')}");`
-)
 
 export const bento = new BentoCache({
   default: 'movies',
   stores: {
     movies: bentostore({ ttl: '24h' })
       .useL1Layer(memoryDriver({ maxSize: 10_000 }))
-      .useL2Layer(
-        redisDriver({
-          connection: redisConnection,
-        })
-      ),
   },
 })
