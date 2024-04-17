74a2b024ef9abc6e810aba4b371997a674bedc08
Matteo Gassend <mgassend@gmail.com>
2024-04-17 14:52:46 +0200

    add: switched turbo for unpoly

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import '../css/app.css'
 import.meta.glob(['../img/**'])
-import * as Turbo from '@hotwired/turbo'
 import Alpine from 'alpinejs'
+import "unpoly"
 window.Alpine = Alpine
 Alpine.start()
+
